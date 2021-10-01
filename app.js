0510203f80dfd28ea394bdb3d362908113d7ba45
Mariia <konvaly@gmail.com>
2021-10-01 21:27:37 +0300

    Add validation message

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 
 const messageController = require('./controllers/message.controller');
+const validate = require('./middleware/validate.mw');
 const app = express();
+
 app.use(express.json());
 
 //Получение всех сообщений
 app.get('/messages', messageController.getMessages);
 
 //Создание нового сообщения
-app.post('/messages', messageController.createMessage);
+app.post(
+  '/messages',
+  validate.validateMessage,
+  messageController.createMessage
+);
 
 //Получение сообщения по id
 app.get('/messages/:messageId', messageController.getMessageById);
@@ -18,5 +24,9 @@ app.delete('/messages/:messageId', messageController.deleteMessage);
 
 //Обновление сообщения по id
 app.patch('/messages/:messageId', messageController.updateMessage);
+
+app.use((err, req, res, next) => {
+  res.status(500).send(err);
+});
 
 module.exports = app;
---
+++ middleware/validate.mw.js
@@ -0,0 +1,24 @@
+module.exports.validateMessage = async (req, res, next) => {
+  const yup = require('yup');
+  const { body } = req;
+
+  const USER_VALIDATION_SCHEMA = yup.object().shape({
+    textOfMessage: yup
+      .string()
+      .min(1)
+      .max(500)
+      .required(),
+    emailOfAutor: yup
+      .string()
+      .email()
+      .required(),
+  });
+
+  try {
+    const validatedMessage = await USER_VALIDATION_SCHEMA.validate(body);
+    req.body = validatedMessage;
+    next();
+  } catch (e) {
+    next(e);
+  }
+};
